86e9320fd000a903ebe9bd058611f4e824b22c6e
Nuno Lau <nunolau@ua.pt>
2023-09-29 10:15:15 +0100

    agentPPO with argparse

--- ciberEnv/agentPPO.py
+++ ciberEnv/agentPPO.py
@@ -1,6 +1,7 @@
 
 from stable_baselines3 import PPO
 import numpy as np
+import argparse
 
 import sys
 sys.path.append('../pClient')
@@ -9,16 +10,23 @@ from croblink import *
 SIM_IP = "127.0.0.1"
 SIM_PORT = 6000
 
-model = PPO.load("ciber_ppo_3")
+parser = argparse.ArgumentParser()
+parser.add_argument('-m','--model', help='model filename', default='ciber_ppo_3')
+parser.add_argument('-s','--server', help='simulator address', default='localhost')
 
-rob = CRobLink("agentPPO", 0, 'localhost')
+args = parser.parse_args()
+
+model = PPO.load(args.model)
+
+rob = CRobLink("agentPPO", 0, args.server)
 
 action = np.array([0.0,0.0])
 while True:
     rob.readSensors()
 
     obsl = [float(x) for x in rob.measures.lineSensor]
-    obs = np.append(np.array(obsl),action)
+    #obs = np.append(np.array(obsl),action)
+    obs = obsl
 
     action, _states = model.predict(obs, deterministic=True)
 
